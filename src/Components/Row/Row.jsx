2c67b69027322a7d0c72dabdfbd55356df064b9d
AnamariaM20 <anamaria.munteanu2024@gmail.com>
2025-04-23 22:35:24 +0300

    added

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import Cell from '../Cell/Cell'
-import "./index.css"
+import React from 'react';
+import Cell from '../Cell/Cell';
+import "./index.css";
 
-const Row = ({item}) => {
+const Row = ({ item }) => {
   return (
-        <tr className='form'>
-        {Object.entries(item).map(([key, value]) =>{
-            return (
-                <Cell key={key} cellData = {JSON.stringify(value)}/>
-            )
-        })}
+    <tr className='form'>
+      {Object.entries(item).map(([key, value]) => {
+        // Când cheia este "address" sau "company"
+        if ((key === "address" || key === "company") && typeof value === "object") {
+          const cells = [];
+
+          Object.values(value).forEach((val, index) => {
+            if (typeof val === 'object') {
+              // Dacă valoarea este un obiect (ex: geo), extragem și valorile din el
+              Object.values(val).forEach((subVal, subIndex) => {
+                cells.push(
+                  <Cell key={`${key}-sub-${index}-${subIndex}`} cellData={subVal} />
+                );
+              });
+            } else {
+              cells.push(
+                <Cell key={`${key}-${index}`} cellData={val} />
+              );
+            }
+          });
+
+          return cells;
+        }
+
+        // Pentru orice alt câmp normal
+        return (
+          <Cell key={key} cellData={typeof value === 'object' ? JSON.stringify(value) : value} />
+        );
+      })}
     </tr>
-  
-   
-    
-  )
-}
-export default Row
+  );
+};
+
+export default Row;
